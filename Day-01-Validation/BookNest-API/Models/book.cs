0cabd59d4143e915dfafeec7c1a07d6ae4016ae9
Naseem aljabri <119049998+Naseem-jabri@users.noreply.github.com>
2026-08-17 00:56:25 +0300

    Add BookNest API for Day 01

---
+++ Day-01-Validation/BookNest-API/Models/book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookNest_API.Models
+{
+    public class book
+    {
+       public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
