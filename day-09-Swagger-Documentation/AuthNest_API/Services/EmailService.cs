978d0cf5474eacfaba1702ffbd7f27acef80e026
Naseem aljabri <119049998+Naseem-jabri@users.noreply.github.com>
2026-09-04 02:53:55 +0300

    Day 09: Add Swagger API documentation and JWT authorization

---
+++ day-09-Swagger-Documentation/AuthNest_API/Services/EmailService.cs
@@ -0,0 +1,50 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace AuthNest_API.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress(
+                "AuthNest",
+                _configuration["EmailSettings:Username"]
+            ));
+
+            email.To.Add(MailboxAddress.Parse(to));
+
+            email.Subject = subject;
+
+            email.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using var smtp = new SmtpClient();
+
+            await smtp.ConnectAsync(
+                _configuration["EmailSettings:Host"],
+                int.Parse(_configuration["EmailSettings:Port"]!)
+            );
+
+            await smtp.AuthenticateAsync(
+                _configuration["EmailSettings:Username"],
+                _configuration["EmailSettings:Password"]
+            );
+
+            await smtp.SendAsync(email);
+
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
---
+++ day-09-Swagger-Documentation/AuthNest_API/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthNest_API.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string subject, string body);
+    }
+}
